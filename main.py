cb9437828f1010f0a4e5537c222e4b664d427926
Talia Cohen <talia13@meet.mit.edu>
2014-03-03 19:49:31 +0200

    main screen

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame 
+
+if __name__=="__main__":
+	pygame.init()
+	main_screen=pygame.display.set_mode((600,600))
+        main_screen.fill((175,238,238))
+        while True:
+		pygame.display.flip()
