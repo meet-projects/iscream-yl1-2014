4dbfbbcb707d86f975e7e1bc0a7090534e2f4fd2
Eli Brown <eli13@meet.mit.edu>
2014-03-24 18:40:52 +0200

    things

--- blocks.py
+++ blocks.py
@@ -1,14 +1,12 @@
 import pygame
-class button(object):
-	def __init__(self, size, location, color):
-		self.size = size
-        	self.location = location
-        	self.color = color
-		self.text = text
-	def create(self):
-		button_rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
-		button_sq = pygame.Surface([self.size[0], self.size[1])
-		button_sq.fill(color[0],color[1],color[2])
-		main_screen.blit(button_sq, buttom_rec) 
-	
-
+class Button(object):
+    def __init__(self, size, location, color):
+        self.size = size
+        self.location = location
+        self.color = color
+        #self.text = text
+    def create(self, main_screen):
+        button_rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1])
+        button_sq = pygame.Surface([self.size[0], self.size[1]])
+        button_sq.fill(self.color)
+        main_screen.blit(button_sq, buttom_rec) 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame 
-
+import blocks
 if __name__=="__main__":
 	pygame.init()
 	main_screen=pygame.display.set_mode((600,600))
-        main_screen.fill((175,238,238))
-        while True:
+    main_screen.fill((175,238,238))
+    while True:
 		pygame.display.flip()
+	a = blocks.Button([10,10], [10,10], [255,0,0])
