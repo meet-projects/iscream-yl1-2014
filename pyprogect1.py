0bdcda34e907008efafa981bc9191de6d9b5e299
Tamir Hanania <tamer13@meet.mit.edu>
2014-03-24 19:15:10 +0200

    picture thingy

--- pyprogect1.py
+++ pyprogect1.py
@@ -15,6 +15,20 @@ class Picture (object):
 
 		buttonimg = pygame.image.load(image_to_set)
 
-		def clear_picture(self):
-			
+	#to_clear_a_picture
+		if__name__=="__main__":
+
+			pygame.init()
+			main_screen = pygame.display.set_mode((600, 600))
+			main_screen.fill((255,255,255))
+			a=blocks.button([70, 50] , [50,50] , (232,148, 172))
+			a.create(main_screen)
+			button_rec = pygame.Rect (100, 200, 50, 20)
+			button_sq = pygame.Surface([20, 20])
+			main_screen.blit(button_sq, button_rec)
+
+			while true:
+				ev = pygame.event.poll()
+
+				pygame.displpay.flip()
 			
