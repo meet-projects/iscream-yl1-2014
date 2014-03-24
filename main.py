ee326d7f5eaed6071dfc887423821a54f622d187
Eli Brown <eli13@meet.mit.edu>
2014-03-24 19:55:01 +0200

    main.py merge conflict

--- main.py
+++ main.py
@@ -8,14 +8,8 @@ def clear_screen():
 if __name__=="__main__":
 	pygame.init()
 	main_screen=pygame.display.set_mode((600,600))
-<<<<<<< HEAD
-	main_screen.fill((175, 238, 238))
-	a=blocks.button([70,50],[50,50], (232,148,172))       
-=======
         main_screen.fill((175,238,238))
-        
-       	a=blocks.button([80,60],[100,200], (232,148,172))       
->>>>>>> a29e29df09f049a23d9cf66298bef56ae4867d31
+       	a=blocks.button([80,60],[100,200], (232,148,172))   
 	a.create(main_screen)
         b=blocks.button([80,60],[250,200], (232,148,172))       
 	b.create(main_screen)
