641e12033c1596e3e69474091615db8ee2eee503
Eli Brown <eli13@meet.mit.edu>
2014-03-31 18:09:31 +0300

    finaly image class is ready

--- pyprogect1.py
+++ pyprogect1.py
@@ -1,3 +1,4 @@
+import pygame
 class Picture (object):
 	def __init__(self, x , y, size, image_to_set):
 		self.x = x
@@ -5,16 +6,10 @@ class Picture (object):
 		self.xdim = size[0]
 		self.ydim = size[1]
 		self.image = image_to_set
+		self.button_rec=pygame.Rect(x, y, size[0], size[1])
+		self.buttonimg = pygame.image.load(image_to_set)
 
 		
-
-		button_rec=pygame.Rect(x, y, size)
-		button_sq=pygame.Surface([y, size])
-		main_screen.blit(button_sq, button_rec)
-		button_sq.fill((0, 255, 0))
-
-		buttonimg = pygame.image.load(image_to_set)
-
-		def clear_picture(self):
-			
-			
+	def drawimage(self, main_screen):
+		main_screen.blit(self.buttonimg, self.button_rec)
+		
