5af02adca952ceb2f9d814e3371563cc6d53b247
Eli Brown <eli13@meet.mit.edu>
2014-03-31 19:00:17 +0300

    label_finished

--- labelclass.py
+++ labelclass.py
@@ -1,16 +1,28 @@
 import pygame
 class Label(object):
-	def __init__(self, location , size, color , font, sizelabels):
+	def __init__(self, location , size, text, fontsize):
 		self.location = location
+		self.fontsize = fontsize
 		self.size = size
-		self.color = color
-		self.font = font	
+		self.text = text
+		self.label_rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1]) 
+		orderlabel = pygame.font.Font(None, self.fontsize)
+		self.label = orderlabel.render(self.text, 1, (0,0,0))	
 
 
 
 	def createlabel(self, main_screen):
-		label_rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1]) 
-		orderlabel = pygame.font.Font(self.font, self.color)
-		label = orderlabel.render()
-		main_screen.blit(label, label_rec)
+		main_screen.blit(self.label, self.label_rec)
 		
+if  __name__ == "__main__":
+	pygame.init()
+	main_screen=pygame.display.set_mode((600,600))
+	main_screen.fill((175,238,238))
+	A = Label([100,100], [50,50], "sdfhgjkdfh", 200)
+	A.createlabel(main_screen)
+
+
+	while True:
+		ev = pygame.event.poll()
+	
+		pygame.display.flip()
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-=======
+
 import pygame
 import blocks 
 import labelclass 
-<<<<<<< HEAD
 
-def clear_screen(r,g,b):
-	button_rec = pygame.Rect(0,0 ,600 ,600 )
-	button_sq = pygame.Surface([20, 20])
-	button_sq.fill((r, g, b))
-	main_screen.blit(button_sq, button_rec)
-
-
-
-
-
-
-
-
-=======
 def clear_screen():
 	
-	
->>>>>>> 43dff2532c701c07701e0c5f95756d49642cd4ad
+
 if __name__=="__main__":
 	pygame.init()
 	main_screen=pygame.display.set_mode((600,600))
@@ -46,4 +29,3 @@ if __name__=="__main__":
 		ev = pygame.event.poll()
 	
 		pygame.display.flip()
->>>>>>> 01682e8e2b7ec98867ca1bfa9c9d57670a5e2729
