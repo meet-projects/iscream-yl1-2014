31280c9103b93b58f2cfaf56c72c66a6f19d444f
Nicole Zeit <nicole13@meet.mit.edu>
2014-03-31 20:01:21 +0300

    four window

---
+++ fourthwindow.py
@@ -0,0 +1,37 @@
+import pygame
+import blocks
+import labelclass
+if __name__=="__main__":
+	pygame.init()
+	main_screen=pygame.display.set_mode((600,600))
+    	main_screen.fill((255,125,186))
+
+
+
+
+
+	a4 = blocks.button([100,100],[100,150],(130,112,255))
+	a4.create(main_screen)
+	la4 = labelclass.Label([100,250],[100,25],"Vanilla",20)
+	la4.createlabel(main_screen)	
+	b4 = blocks.button([100,100],[400,150],(130,112,255))
+	b4.create(main_screen)
+	lb4 = labelclass.Label([400,250],[100,25],"Chocolate",20)
+	lb4.createlabel(main_screen)	
+	c4 = blocks.button([100,100],[250,300],(130,112,255))
+	c4.create(main_screen)
+	lc4 = labelclass.Label([250,400],[100,25],"Strawberry",20)
+	lc4.createlabel(main_screen)	
+	d4 = blocks.button([120,50],[150,550],(130,112,255))
+	d4.create(main_screen)
+	ld4 = labelclass.Label([150,550],[120,50],"Previous", 30)
+	ld4.createlabel(main_screen)
+	e4 = blocks.button([120,50],[300,550],(130,112,255))
+	e4.create(main_screen)
+	le4 = labelclass.Label([300,550],[120,50],"Next", 30)
+	le4.createlabel(main_screen)
+
+	while True:
+		ev = pygame.event.poll()
+	
+		pygame.display.flip()
