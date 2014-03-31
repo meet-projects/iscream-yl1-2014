64e6ad2c98f090401dde3d3a860c99d8cf2f85a3
Eli Brown <eli13@meet.mit.edu>
2014-03-31 18:31:11 +0300

    fjksdhf

--- labelclass.py
+++ labelclass.py
@@ -1,16 +1,16 @@
 import pygame
 class Label(object):
-	def __init__(self, location_ts , size_ts, color_ts , font_ts, sizelabels_ts):
-		self.location = location_ts
-		self.size = size_ts
-		self.color = color_ts
-		self.font = font_ts	
+	def __init__(self, location , size, color , font, sizelabels):
+		self.location = location
+		self.size = size
+		self.color = color
+		self.font = font	
 
 
 
-	def createlabel(self):
-		label_rec = pygame.Rect(self.location_ts[0], self.location_ts[1], self.size_ts[0], self.size_ts[1]) 
-		orderlabel = pygame.font.Font(self.font_ts,self.sizelabels_ts, color_ts)
+	def createlabel(self, main_screen):
+		label_rec = pygame.Rect(self.location[0], self.location[1], self.size[0], self.size[1]) 
+		orderlabel = pygame.font.Font(self.font, self.color)
 		label = orderlabel.render()
 		main_screen.blit(label, label_rec)
 		
