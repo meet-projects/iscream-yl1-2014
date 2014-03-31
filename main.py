10aaed5be4b068ebcf997fddd35e33a810e43ecc
Talia Cohen <talia13@meet.mit.edu>
2014-03-31 18:28:17 +0300

    label_not_finished

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import pygame
 import blocks 
-import lableclass 
-def clear_screen():
-	
-	
+import labelclass 
+
+def clear_screen(r,g,b):
+	button_rec = pygame.Rect(0,0 ,600 ,600 )
+	button_sq = pygame.Surface([20, 20])
+	button_sq.fill((r, g, b))
+	main_screen.blit(button_sq, button_rec)
+
+
+
+
+
+
+
 
 if __name__=="__main__":
 	pygame.init()
 	main_screen=pygame.display.set_mode((600,600))
-        main_screen.fill((175,238,238))
-       	a=blocks.button([80,60],[100,200], (232,148,172))   
+    main_screen.fill((175,238,238))
+
+ 
+
+   	a=blocks.button([80,60],[100,200], (232,148,172))   
 	a.create(main_screen)
-        b=blocks.button([80,60],[250,200], (232,148,172))       
+    b=blocks.button([80,60],[250,200], (232,148,172))       
 	b.create(main_screen)
-        c=blocks.button([80,60],[400,200], (232,148,172))       
+    c=blocks.button([80,60],[400,200], (232,148,172))       
 	c.create(main_screen)
-        
+	d=blocks.button([80,60],[250,400], (232,148,172))       
+	d.create(main_screen)
+
+		
+
+
 	while True:
 		ev = pygame.event.poll()
 	
