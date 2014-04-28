5d36bea283013abfb445b155b4c7a97d019de54a
Talia Cohen <talia13@meet.mit.edu>
2014-04-28 19:42:12 +0300

    welcome screen

--- welcomscreen.py
+++ welcomscreen.py
@@ -1,13 +1,27 @@
 import pygame
+import picture 
+import blocks
+import labelclass
+
+def draw_screen(main_screen):
+    main_screen.fill((0,0,0))
+
+    picture1= picture.Picture(105,15,[250,270],"Link to finallogo.jpg")
+    picture1.draw(main_screen)
+
+    p1=blocks.button([60,40],[510,550], (232,148,172))   
+    p1.create(main_screen)
+    lp1=labelclass.Label([510,550],[60,40],"Order",25)
+    lp1.createlabel(main_screen)
+
+
 
 if __name__=="__main__":
     pygame.init()
     main_screen=pygame.display.set_mode((600,600))
-    main_screen.fill((255,102,102))
+    draw_screen(main_screen)
 
-    a1=blocks.button([80,60],[250,500], (232,148,172))   
-    a1.create(main_screen)
 
     while True:
-    	ev = pygame.event.poll()
-    	pygame.display.flip()
+        ev = pygame.event.poll()
+        pygame.display.flip()
